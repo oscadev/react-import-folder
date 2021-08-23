a151422f9a54b3f347419031e098244fb1fdaa66
devosc <devosc@devoscs-MacBook-Air.local>
2021-08-23 13:13:32 -0600

    added recursion option, plus new object key 'location' to seperate by folder if needed.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
-let path = process.argv[2] || null;
+
 const fs = require("fs");
+
+String.prototype.replaceAtIndex = function (_index, _newValue) {
+  return (
+    this.substr(0, _index) + _newValue + this.substr(_index + _newValue.length)
+  );
+};
+
+let path = process.argv[2] || null;
+let recursive = false;
+if (process.argv[3] == "all") {
+  recursive = true;
+}
 
 //strip potential ending forward slash
 if (path[path.length - 1] === "/") {
@@ -25,62 +37,80 @@ const importThese = [
   "eps",
   "pdf",
 ];
-String.prototype.replaceAtIndex = function (_index, _newValue) {
-  return (
-    this.substr(0, _index) + _newValue + this.substr(_index + _newValue.length)
-  );
-};
 
 if (!path) {
   console.log(
-    "You didnt add a path. Try again and add the path name. For example: node importer /Users/me/dev/project2/src/assets/img"
+    "You didnt add a path. Try again and add the relative path to the folder with the images. For example: react-import-folder ./src/assets/img\nadd 'all' to look in subdirectories too. For example: react-import-folder ./src/assets/img all"
   );
   process.exit();
 }
 
 let output = "";
+let imports = "";
 let data = "";
+let dataOBJ = {};
 let forbidden = ["-", " ", "", ".", "&"];
-fs.readdir(path, function (err, files) {
+
+function main(pth) {
+  console.log(pth);
+  console.log(pth.split("/"));
+  files = fs.readdirSync(pth);
+  // fs.readdir(pth, function (err, files) {
   //handling error
-  if (err) {
-    return console.log("Unable to scan directory: " + err);
-  }
+  // if (err) {
+  //   return console.log("Unable to scan directory: " + err);
+  // }
   //listing all files using forEach
   files.forEach(function (file) {
-    //check if valid file extension
-    let lastPeriodPosition = file.lastIndexOf(".");
-    let ext = file.slice(lastPeriodPosition + 1);
-    //only include if valid extension
-    if (importThese.includes(ext)) {
-      let len = file.length;
-      // Convert name to var
-      let validVarName, filenameWithoutExt;
+    //check if file is a folder and that they want recursion
+    if (fs.lstatSync(pth + "/" + file).isDirectory() && recursive) {
+      main(pth + "/" + file);
+    } else {
+      //check if valid file extension
+      let lastPeriodPosition = file.lastIndexOf(".");
+      let ext = file.slice(lastPeriodPosition + 1).toLocaleLowerCase();
+      //only include if valid extension
+      if (importThese.includes(ext)) {
+        //create key for folder in data object
 
-      //find extension and remove
+        let len = file.length;
+        // Convert name to var
+        let validVarName, validPathKeyName, filenameWithoutExt;
 
-      validVarName = file.slice(0, len - (len - lastPeriodPosition));
-      filenameWithoutExt = validVarName;
+        //find extension and remove
 
-      //replace forbidden chars with underscore
-      for (let i = 0; i < validVarName.length; i++) {
-        if (forbidden.includes(validVarName[i])) {
-          validVarName = validVarName.replaceAtIndex(i, "_");
-        }
+        validVarName = file.slice(0, len - (len - lastPeriodPosition));
+        filenameWithoutExt = validVarName;
+
+        validVarName = validVarNameMaker(validVarName);
+        validPathKeyName = validVarNameMaker(pth);
+
+        imports += `import ${validVarName} from '${pth}/${file}'\n`;
+
+        data += `{img: ${validVarName},\n name: "${filenameWithoutExt}", folder: "${pth}"},\n`;
       }
-      validVarName = "_" + validVarName;
-      // console.log(validVarName);
-
-      output += `import ${validVarName} from '${path}/${file}'\n`;
-
-      data += `{img: ${validVarName},\n name: "${filenameWithoutExt}"},\n`;
     }
   });
 
-  output += `\n\nconst data = [\n${data}\n]\nexport default data;`;
+  // });
+}
 
-  fs.writeFile("imgImports.js", output, function (err) {
-    if (err) return console.log(err);
-    console.log("imgImports.js has been created.");
-  });
+main(path);
+
+function validVarNameMaker(str) {
+  //replace forbidden chars with underscore
+  for (let i = 0; i < str.length; i++) {
+    if (forbidden.includes(str[i])) {
+      str = str.replaceAtIndex(i, "_");
+    }
+  }
+  str = "_" + str;
+  return str;
+}
+
+output += `${imports}\n\nconst data = [\n${data}\n]\nexport default data;`;
+
+fs.writeFile("imgImports.js", output, function (err) {
+  if (err) return console.log(err);
+  console.log("imgImports.js has been created.");
 });
